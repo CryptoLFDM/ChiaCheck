381dd7e4b34c709b13a3f09a9bfdc8a68164e543
Moz <contact@ether-source.fr>
2023-04-27 19:18:36 +0200

    fix string filename

--- check_plot.py
+++ check_plot.py
@@ -3,7 +3,8 @@ from chia.plotting.check_plots import check_plots
 import logging
 import sys
 
+
 def check_plot(challenge: int, chia_root_path: str):
-    logging.basicConfig(filename=sample.log, stream=sys.stdout, level=logging.INFO)
+    logging.basicConfig(filename='sample.log', stream=sys.stdout, level=logging.INFO)
     path = Path(chia_root_path)
     check_plots(path, challenge, None, "", True, False)
