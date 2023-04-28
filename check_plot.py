000edcfa6a0785a828aececdc87e5c4221c73f4a
Moz <contact@ether-source.fr>
2023-04-28 19:31:46 +0200

    fix filemode for log

--- check_plot.py
+++ check_plot.py
@@ -5,7 +5,7 @@ from dissect_log import dissect_log
 
 
 def check_plot(challenge: int, chia_root_path: str, pseudo: str):
-    logging.basicConfig(filename='sample.log', level=logging.INFO)
+    logging.basicConfig(filename='sample.log', filemode='w', level=logging.INFO)
     path = Path(chia_root_path)
     check_plots(path, challenge, None, "", True, False)
     dissect_log(pseudo, challenge)
